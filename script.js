74ad87d8bc24635760f13dcc0eb66a2f1d3ffffe
Karan sapra <88654249+CraveforSuccess@users.noreply.github.com>
2022-09-15 21:10:04 +0530

    Update script.js
    Removed the file explorer marks in images

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var img1 = document.getElementById("img1");
-img1.setAttribute("src","./images/dice6.png");
+img1.setAttribute("src","dice6.png");
 var img2 = document.getElementById("img2");
-img2.setAttribute("src","./images/dice6.png");
+img2.setAttribute("src","dice6.png");
 
 
 
@@ -14,27 +14,27 @@ console.log(random_num);
 
 if(random_num==1){
 
-    img1.setAttribute("src","./images/dice1.png")
+    img1.setAttribute("src","dice1.png")
 }
 if(random_num==2){
 
-    img1.setAttribute("src","./images/dice2.png")
+    img1.setAttribute("src","dice2.png")
 }
 if(random_num==3){
 
-    img1.setAttribute("src","./images/dice3.png")
+    img1.setAttribute("src","dice3.png")
 }
 if(random_num==4){
 
-    img1.setAttribute("src","./images/dice4.png")
+    img1.setAttribute("src","dice4.png")
 }
 if(random_num==5){
 
-    img1.setAttribute("src","./images/dice5.png")
+    img1.setAttribute("src","dice5.png")
 }
 if(random_num==6){
 
-    img1.setAttribute("src","./images/dice6.png")
+    img1.setAttribute("src","dice6.png")
 }
 // img2 images
 var random_num1 = Math.random();
@@ -43,27 +43,27 @@ random_num1 = Math.floor(random_num1) + 1;
 
 if(random_num1==1){
 
-    img2.setAttribute("src","./images/dice1.png")
+    img2.setAttribute("src","dice1.png")
 }
 if(random_num1==2){
 
-    img2.setAttribute("src","./images/dice2.png")
+    img2.setAttribute("src","dice2.png")
 }
 if(random_num1==3){
 
-    img2.setAttribute("src","./images/dice3.png")
+    img2.setAttribute("src","dice3.png")
 }
 if(random_num1==4){
 
-    img2.setAttribute("src","./images/dice4.png")
+    img2.setAttribute("src","dice4.png")
 }
 if(random_num1==5){
 
-    img2.setAttribute("src","./images/dice5.png")
+    img2.setAttribute("src","dice5.png")
 }
 if(random_num1==6){
 
-    img2.setAttribute("src","./images/dice6.png")
+    img2.setAttribute("src","dice6.png")
 }
 
 if(random_num>random_num1){
